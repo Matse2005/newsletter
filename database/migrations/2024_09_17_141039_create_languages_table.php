011122478d028a7a4c0de188577670e33befd7b0
MatseVH <49431416+Matse2005@users.noreply.github.com>
2024-09-17 23:07:44 +0200

    Made it insert the default language on migration

--- database/migrations/2024_09_17_141039_create_languages_table.php
+++ database/migrations/2024_09_17_141039_create_languages_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -19,6 +20,45 @@ return new class extends Migration
             $table->json('translations');
             $table->timestamps();
         });
+
+        // Insert default row(s) into the languages table
+        DB::table('languages')->insert([
+            [
+                'key' => 'default',
+                'title' => 'Default',
+                'local' => 'Default',
+                'translations' => json_encode([
+                    "email" => [
+                        "footer" => [
+                            "company" => "Your Company Heuvelstraat 106/2 3390 Tielt-Winge Belgium",
+                            "in_browser" => "Open this newsletter in the browser",
+                            "click_here" => "click here",
+                            "send_to" => "This email has been sent to",
+                            "privacy_statement" => "Privacy Statement",
+                            "tos" => "Terms & Conditions",
+                            "unsubscibe" => "Unsubscribe",
+                            "urls" => [
+                                "privacy_statement" => "https://www.dezittere-philac.be/content/2-privacy-statement",
+                                "tos" => "https://www.dezittere-philac.be/content/3-algemene-voorwaarden"
+                            ]
+                        ]
+                    ],
+                    "pages" => [
+                        "unsubscribe" => [
+                            "title" => "Dezittere Philac",
+                            "unsubscribe" => "Unsubscribe",
+                            "description" => "You have successfully unsubscribed from our newsletter. If you'd like to receive our updates again, you can re-subscribe at any time.",
+                            "subscribe_again" => "Subscribe Again",
+                            "url" => [
+                                "subscribe_again" => "https://www.dezittere-philac.be/#footer"
+                            ]
+                        ]
+                    ]
+                ]),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 
     /**
