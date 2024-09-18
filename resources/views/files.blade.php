bf34a0dcc887a4dde7c9c09937720b1e540177e2
MatseVH <49431416+Matse2005@users.noreply.github.com>
2024-09-18 17:11:27 +0200

    Added a button to navigate to the create file page when searching for the files

--- resources/views/files.blade.php
+++ resources/views/files.blade.php
@@ -1,3 +1,18 @@
-<div class="">
-    <livewire:file.list-files />
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Styles -->
+    {{-- @vite('resources/css/app.css') --}}
+</head>
+
+<body>
+    <div class="">
+        <livewire:file.list-files />
+    </div>
+</body>
+
+</html>
--- resources/views/livewire/file/list-files.blade.php
+++ resources/views/livewire/file/list-files.blade.php
@@ -1,3 +1,17 @@
-<div>
+<div class="flex flex-col space-y-6">
+    <div class="flex justify-end w-full">
+        <a href="{{ route('filament.newsletter.resources.files.create') }}" target="_blank" rel="noopener noreferrer"
+            class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action "
+            style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600);">
+            <svg wire:target="mountAction('bestanden')" class="w-5 h-5 text-white transition duration-75 fi-btn-icon"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" aria-hidden="true" data-slot="icon">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z">
+                </path>
+            </svg>
+            <span class="fi-btn-label">Bestand toevoegen</span>
+        </a>
+    </div>
     {{ $this->table }}
 </div>
