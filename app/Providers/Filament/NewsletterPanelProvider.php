36c19ed7ad458494b79212968d5938d3b46404df
MatseVH <49431416+Matse2005@users.noreply.github.com>
2024-09-18 21:31:19 +0200

    Made a difference between primary and danger

--- app/Providers/Filament/NewsletterPanelProvider.php
+++ app/Providers/Filament/NewsletterPanelProvider.php
@@ -30,6 +30,7 @@ class NewsletterPanelProvider extends PanelProvider
             ->login()
             ->colors([
                 'primary' => Color::Red,
+                'danger' => Color::Rose,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
